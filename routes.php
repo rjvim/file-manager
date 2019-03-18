5bf42034cc9a5eaa3d75a97ffebe604ec11a76bb
Rajiv Seelam <rajiv@betalectic.com>
2019-03-18 17:00:24 +0530

    Remove prefix to support lumen

--- routes.php
+++ routes.php
@@ -1,16 +1,13 @@
 <?php
 
-Route::prefix('file-manager')->group(function () {
+Route::namespace('Betalectic\FileManager\Http\Controllers')->group(function () {
 
-	Route::namespace('Betalectic\FileManager\Http\Controllers')->group(function () {
-
-        Route::get('tags', 'FileController@tags');
-        Route::get('/', 'FileController@index');
-        Route::delete('/{id}', 'FileController@destroy');
-        Route::put('/{id}', 'FileController@update');
-        Route::post('/upload-save', 'UploadSaveController@store');
-		
-    });
+    Route::get('file-manager/tags', 'FileController@tags');
+    Route::get('file-manager', 'FileController@index');
+    Route::delete('file-manager/{id}', 'FileController@destroy');
+    Route::put('file-manager/{id}', 'FileController@update');
+    Route::post('file-manager/upload-save', 'UploadSaveController@store');
+	
 });
 
 
