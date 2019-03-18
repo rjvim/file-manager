36d37ae9d65b6bbad36ba710cdc93380e5ab4f83
Rajiv Seelam <rajiv@betalectic.com>
2019-03-18 17:04:03 +0530

    Remove namespace to support lumen

--- routes.php
+++ routes.php
@@ -1,13 +1,11 @@
 <?php
 
-Route::namespace('Betalectic\FileManager\Http\Controllers')->group(function () {
 
-    Route::get('file-manager/tags', 'FileController@tags');
-    Route::get('file-manager', 'FileController@index');
-    Route::delete('file-manager/{id}', 'FileController@destroy');
-    Route::put('file-manager/{id}', 'FileController@update');
-    Route::post('file-manager/upload-save', 'UploadSaveController@store');
+    Route::get('file-manager/tags', 'Betalectic\FileManager\Http\Controllers\FileController@tags');
+    Route::get('file-manager', 'Betalectic\FileManager\Http\Controllers\FileController@index');
+    Route::delete('file-manager/{id}', 'Betalectic\FileManager\Http\Controllers\FileController@destroy');
+    Route::put('file-manager/{id}', 'Betalectic\FileManager\Http\Controllers\FileController@update');
+    Route::post('file-manager/upload-save', 'Betalectic\FileManager\Http\Controllers\UploadSaveController@store');
 	
-});
 
 
