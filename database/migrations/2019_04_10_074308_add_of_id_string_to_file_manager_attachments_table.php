23b2b9f434abb136c21ecc85e5132227dc506cf0
Rajiv Seelam <rajiv@betalectic.com>
2019-04-10 13:15:04 +0530

    Make of_id string

---
+++ database/migrations/2019_04_10_074308_add_of_id_string_to_file_manager_attachments_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOfIdStringToFileManagerAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE "public"."file_manager_attachments" ALTER COLUMN "of_id" TYPE character varying(255)');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('ALTER TABLE "public"."file_manager_attachments" ALTER COLUMN "of_id" TYPE integer');
+    }
+}
